ecf913fa8e519a4dfbf2fa2389d7b21811e23130
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 11:16:23 +0100

    retrait app/bundle

--- src/AB/CoreBundle/Controller/DefaultController.php
+++ src/AB/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ABCoreBundle:Default:index.html.twig');
+        return $this->render('ABCoreBundle:layout.html.twig');
     }
 }
