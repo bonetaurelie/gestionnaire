d1e0551dc506eb8223e1a7ddf3d4c3a74ee65bc4
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 11:34:02 +0100

    creation du controller PlatformBundle

---
+++ src/AB/PlatformBundle/Controller/BookController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AB\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BookController extends Controller
+{
+    public function AccueilAction()
+    {
+
+    }
+
+    public function addAction(){
+
+    }
+
+    public function deleteAction($id){
+
+    }
+
+    public function updateAction($id){
+
+    }
+
+    public function read(){
+        
+    }
+}
