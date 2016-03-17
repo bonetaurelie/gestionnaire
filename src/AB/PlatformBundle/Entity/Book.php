cda2e5fcb550abb423d772d25e701b38c8751769
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 13:45:59 +0100

    creation entité

---
+++ src/AB/PlatformBundle/Entity/Book.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AB\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="AB\PlatformBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="text", unique=true)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float")
+     */
+    private $prix;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantite_dispo", type="integer")
+     */
+    private $quantiteDispo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     * @return Book
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     * @return Book
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string 
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param float $prix
+     * @return Book
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return float 
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set quantiteDispo
+     *
+     * @param integer $quantiteDispo
+     * @return Book
+     */
+    public function setQuantiteDispo($quantiteDispo)
+    {
+        $this->quantiteDispo = $quantiteDispo;
+
+        return $this;
+    }
+
+    /**
+     * Get quantiteDispo
+     *
+     * @return integer 
+     */
+    public function getQuantiteDispo()
+    {
+        return $this->quantiteDispo;
+    }
+}
