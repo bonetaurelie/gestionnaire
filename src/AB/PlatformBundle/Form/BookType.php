7c0c5c2a7c211ab7cc67950bb777c67b7a156e08
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 13:48:52 +0100

    Création du formulaire

---
+++ src/AB/PlatformBundle/Form/BookType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AB\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('auteur')
+            ->add('prix')
+            ->add('quantiteDispo')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AB\PlatformBundle\Entity\Book'
+        ));
+    }
+}
