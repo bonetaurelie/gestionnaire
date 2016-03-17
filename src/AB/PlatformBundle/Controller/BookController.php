28583e603042ae9972f7d4de725641d6af41eb42
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 13:46:43 +0100

    maj

--- src/AB/PlatformBundle/Controller/BookController.php
+++ src/AB/PlatformBundle/Controller/BookController.php
@@ -7,16 +7,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BookController extends Controller
 {
-    public function accueilAction($page)
-    {
-        if($page<2){
-            throw new NotFoundHttpException('Page"'.$page.'" inexistante.');
-        }
-            return $this->render('ABPlatformBundle:Book:accueil.html.twig');
+    public function addAction(Request $request){
 
-    }
-
-    public function addAction(){
 
     }
 
