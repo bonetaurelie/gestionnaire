97c0c0568feb815c8844855de77afbe45b0671ed
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-17 12:01:07 +0100

    màj

--- src/AB/PlatformBundle/Controller/BookController.php
+++ src/AB/PlatformBundle/Controller/BookController.php
@@ -8,7 +8,7 @@ class BookController extends Controller
 {
     public function AccueilAction()
     {
-        render('ABPlatformBundle:accueil.html.twig');
+       return $this->render('ABPlatformBundle:accueil.html.twig');
     }
 
     public function addAction(){
