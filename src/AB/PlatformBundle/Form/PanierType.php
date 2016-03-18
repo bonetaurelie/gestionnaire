e92852f9e22d6c5f892f32474d189378f1c42a18
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-18 14:03:47 +0100

    creation du formulaire panier

---
+++ src/AB/PlatformBundle/Form/PanierType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AB\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('auteur')
+            ->add('prix')
+            ->add('quantite')
+            ->add('save','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+
+    public function getName()
+    {
+        return 'ab_platformbundle_book_ajout';
+    }
+
+    public function getParent()
+    {
+        return new BookType();
+    }
+}
