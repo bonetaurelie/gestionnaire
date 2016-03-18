9a0e52cdcabc0220cf5bc6af35230c49b829b9df
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-18 09:14:41 +0100

    creation du formulaire book search

---
+++ src/AB/PlatformBundle/Form/BookSearchType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AB\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookSearchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('auteur')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AB\PlatformBundle\Entity\BookSearch'
+        ));
+    }
+}
