4a9571962e854da4bb0c20ced11acb3f801ff3a4
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-18 12:19:34 +0100

    maj entité

--- src/AB/PanierBundle/Entity/Panier.php
+++ src/AB/PanierBundle/Entity/Panier.php
@@ -24,7 +24,7 @@ class Panier
     /**
      * @var string
      *
-     * @ORM\Column(name="titre", type="string", length=255)
+     * @ORM\Column(name="titre", type="text", unique=false)
      */
     private $titre;
 
