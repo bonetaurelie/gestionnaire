4e7042d46d5bd0b01a843cc55d0af7b29dea3482
bonetaurelie <bonetaurelie@yahoo.fr>
2016-03-18 09:12:20 +0100

    creation de l'entité book search

---
+++ src/AB/PlatformBundle/Entity/BookSearch.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AB\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BookSearch
+ *
+ * @ORM\Table(name="book_search")
+ * @ORM\Entity(repositoryClass="AB\PlatformBundle\Repository\BookSearchRepository")
+ */
+class BookSearch
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     * @return BookSearch
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string 
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+}
